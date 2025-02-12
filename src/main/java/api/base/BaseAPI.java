5338f2dd4c3e8db73ce5223350e07e4236cf4360
unknown <bluebay777@gmail.com>
2025-02-12 12:04:42 +0100

    first commit

---
+++ src/main/java/api/base/BaseAPI.java
@@ -0,0 +1,17 @@
+package api.base;
+
+import api.utils.ConfigManager;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import utils.ConfigReader;
+
+public class BaseTest {
+    static {
+        String environment = System.getProperty("env", "dev");
+        RestAssured.baseURI = ConfigManager.getProperty("baseUri." + environment);
+
+        // Enable logging for debugging
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+}
+}
